8c22e7bb4ca7a6f7493c7154ddbf054f945b5278
loudboy10 <loudboy@sbcglobal.net>
2025-12-31 12:29:11 -0500

    added rear camera

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -84,7 +84,7 @@ def generate_launch_description():
     ros_gz_image_bridge = Node(
         package="ros_gz_image",
         executable="image_bridge",
-        arguments=["/camera/image_raw"]
+        arguments=["/rear_camera/image_raw"]
     )
 
     diff_drive_spawner = Node(
